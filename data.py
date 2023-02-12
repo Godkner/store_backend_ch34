152bedfa84381d3bf0433a5aed85a2884257b9a9
Kevin Fierro <kfierro@uabc.edu.mx>
2023-02-11 19:30:47 -0800

    Class 3

--- data.py
+++ data.py
@@ -11,3 +11,75 @@ me= {
     } 
     
 }
+mock_catalog =[
+    {
+        "title": "Ducky one 3 mini black 60%",
+        "category": "Keyboard",
+        "price": 119.00,
+        "image": "Ducky3black.jpg",
+        "_id": "00001"
+    },
+    {
+        "title": "Ducky one 3 mini white 60%",
+        "category": "Keyboard",
+        "price": 119.00,
+        "image": "Ducky3white.jpg",
+        "_id": "00002"
+    },
+    {
+        "title": "Ducky one 3 matcha TKL Hotswap",
+        "category": "Keyboard",
+        "price": 109.00,
+        "image": "Ducky3matcha.jpg",
+        "_id": "00003"
+    },
+    {
+        "title": "Vortex Race 3 RGB LED TKL ",
+        "category": "Keyboard",
+        "price": 159.00,
+        "image": "vortex3.jpg",
+        "_id": "00004"
+    },
+    {
+        "title": "108 Key PBT Seamless Double Shot Keycap Set- Joker (Ducky)",
+        "category": "Keycaps",
+        "price": 54.00,
+        "image": "joker.jpg",
+        "_id": "00005"
+    },
+    {
+        "title": "22 Key TPR Backlit Double Shot Keycap Set-MK Purple(Tai-Hao)",
+        "category": "Keycaps",
+        "price": 26.00,
+        "image": "taihao.jpg",
+        "_id": "00006"
+    },
+    {
+        "title": "4 key TPR Rubber Backlit Keycap Set Row 1 -ZXCV Neon Blue (Tai-Hao)",
+        "category": "Keycaps",
+        "price": 10.00,
+        "image": "4key.png",
+        "_id": "00007"
+    },
+    {
+        "title": "Cherry MX Red Switches PCB Mount- Linear- 10 Pack (Cherry)",
+        "category": "Switches",
+        "price": 3.99,
+        "image": "redmx.png",
+        "_id": "00008"
+    },
+    {
+        "title": "Cherry MX Blue Switches PCB Mount- Tactile, Click- 10 Pack (Cherry)",
+        "category": "Switches",
+        "price": 3.99,
+        "image": "bluemx.png",
+        "_id": "00009"
+    },
+    {
+        "title": "Cherry MX Brown Switches PCB Mount- Tactile - 10 Pack (Cherry)",
+        "category": "Switches",
+        "price": 3.99,
+        "image": "brownmx.png",
+        "_id": "00010"
+    }
+]
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from data import me
+from flask import Flask,abort
+from data import me,mock_catalog
+
 
 import json
 
@@ -32,4 +33,68 @@ def dev_adress():
     #f string
     return f'{address["street"]}, #{address["city"]}, {address["zipcode"]}'
 
+#get/api/catalog
+# return the list of products as JSON
+@app.get("/api/catalog")
+def get_catalog():
+    return json.dumps(mock_catalog)
+
+#get/api/count
+# return the number of products on the list
+@app.get("/api/catalog/count")
+def count_products():
+    count = len(mock_catalog)
+    return json.dumps(count)
+
+@app.get("/api/category/<cat>")
+def prods_by_category(cat):
+    results=[]
+    for prod in mock_catalog:
+        if prod["category"]==cat:
+            results.append(prod)
+            
+    return json.dumps(results)
+            
+# create an endpoint that allows to retrieve a product by its _id
+# the endpoint should be a get on /api/product/5 where is an _id
+
+@app.get("/api/product/<id>")
+def prod_by_id(id):
+    #find the product with _id equal to id
+    # return it as jsom string
+    for prod in mock_catalog:
+        if prod["_id"] ==id:
+            return json.dumps(prod)
+    
+    #not found
+    return abort(404, "Invalid id")
+
+# get/api/product/search/xyz
+# search products whose title CONTAINS xyz
+
+@app.get("/api/product/search/<text>")
+def prod_search(text):
+    results=[]
+    for prod in mock_catalog:
+        if text.lower() in prod["title"].lower():
+            results.append(prod) 
+    return json.dumps(results)
+
+ 
+
+# @app.get("/api/categories")
+# def get_categories():
+#     results=[]
+#     for prod in mock_catalog:
+#         cat = prod["category"]
+#         if cat==category:
+#             results.append(prod["category"])
+    
+#     res= set(results)
+#     return json.dumps(res)
+            
+            
+        
+            
+    
 app.run(debug=True)
